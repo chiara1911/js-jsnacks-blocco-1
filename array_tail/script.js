f803adb8453d08364e071c169f0e91133be4c9e5
chiara1911 <chiaracesari89@gmail.com>
2023-10-13 16:59:54 +0200

    console.log btn-number

--- array_tail/script.js
+++ array_tail/script.js
@@ -1,6 +1,20 @@
 // Creiamo un array chiedendo all'utente quanti elementi dovrà contenere e generando tanti numeri casuali da 1 a 100 per quanti sono gli elementi da inserire.
+
+
+
 // Stampiamo poi gli ultimi 5 elementi dell'Array
 // Bonus:
 // Chiediamo all'utente quanti elementi dell'array dovremo stampare
 // Suggerimenti:
 // Probabilmente conviene controllare che il numero di elementi della coda non sia più grande del numero di elementi totali dell'array
+
+// 1. chiedere quanti elementi
+// 2. in base a quanti sono, metterli nella lista numeri random da 1 a 100
+
+const number = document.getElementById('number');
+const btn = document.querySelector('button');
+
+
+
+console.log(number);
+console.log(btn);
