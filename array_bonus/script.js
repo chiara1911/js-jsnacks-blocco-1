64cb00053c576ac7d806a6913669bcc048cea4a1
chiara1911 <chiaracesari89@gmail.com>
2023-10-14 13:38:30 +0200

    fix js to array-bonus function and inner

--- array_bonus/script.js
+++ array_bonus/script.js
@@ -4,28 +4,51 @@
 // Crea due array che hanno un numero di elementi diversi.
 // Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
 
-const bean = [];
-
-const luna = [];
 
 
-const beanEl = parseInt(document.getElementById('beanEl').value);
-const lunaEl = parseInt(document.getElementById('lunaEl').value);
+
 const btn = document.querySelector('button');
 
-btn.addEventListener('click', function (){
 
-     let delta = bean.length - luna.length;
-    for (let i = 0 ; i < delta ; i++){
 
-        luna.push(bean[i]);
-      
+btn.addEventListener('click', function () {
+    const beanEl = parseInt(document.getElementById('beanEl').value);
+const lunaEl = parseInt(document.getElementById('lunaEl').value);
+const lotto = document.getElementById('lotto');
+
+
+const bean = [];
+const luna = [];
+    for (let i = 0; i < beanEl; i++) {
+        bean.push(i);
+
     }
-    console.log(luna);
+    for (let i = 0; i < lunaEl; i++) {
+        luna.push(i);
+
+    }
+   
+
+
+    if (beanEl === lunaEl) {
+        text = 'elementi uguali';
+    } else if (beanEl > lunaEl) {
+        let differenza = beanEl - lunaEl;
+        for (let i = 0; i < differenza; i++) {
+            luna.push(bean[i]); 
+            text = 'Questi sono gli elementi resi di ugual misura ' + 'luna = ' + luna + + ' bean = ' + bean;
+        }
+       
+    } else {
+        let differenza = lunaEl - beanEl;
+        for (let i = 0; i < differenza; i++) {
+            bean.push(luna[i]);
+            text = 'Questi sono gli elementi resi di ugual misura ' + 'luna = ' + luna +  ' bean = ' + bean;
+        }
+    }
+
+     lotto.innerHTML = text;
 });
 
 
-// utility
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+
